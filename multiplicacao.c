ce4ebbaaf9aaa7d75ed1d956fb97cc3e1490eb68
Giu <120753646+Giu-11@users.noreply.github.com>
2025-05-25 19:03:10 -0300

    ✨feat: funções de teste com números de 16 bits

--- multiplicacao.c
+++ multiplicacao.c
@@ -6,13 +6,42 @@
 int mult_tmp(int8_t arr1[5][5], int8_t arr2[5][5]){
     for(int i=0; i<5; i++){
         for(int j=0; j<5; j++){
-            arr1[i][j] = (arr1[i][j]) * (arr2[i][j]);
+            int16_t temp = (arr1[i][j]) * (arr2[i][j]);
+            if(temp>127){
+                arr1[i][j] = (int8_t)127;
+            }else if(temp<-128){
+                arr1[i][j] = (int8_t)-128;
+            }else{
+                arr1[i][j] = (int8_t)temp;
+            }
+        } 
+    }
+    return 0;
+}
+
+int mul_completo(int8_t arr1[5][5], int8_t arr2[5][5], int16_t arr3[5][5]){
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            arr3[i][j] = (arr1[i][j]) * (arr2[i][j]);
         } 
     }
     return 0;
 }
 
 int print_matrix5x5(int8_t arr1[5][5]){
+    //printf("\n[\n");
+    for(int i=0; i<5; i++){
+        printf("[");
+        for(int j=0; j<5; j++){
+            printf("%4d ", arr1[i][j]);
+        }
+        printf("]\n");
+    }
+    //printf("]\n");
+    return 0;
+}
+
+int print_matrix5x5_16b(int16_t arr1[5][5]){
     //printf("\n[\n");
     for(int i=0; i<5; i++){
         printf("[");
--- multiplicacao.h
+++ multiplicacao.h
@@ -5,7 +5,11 @@
 
 //multiplica duas matrizes5x5 de 8 bits
 int mult_tmp(int8_t arr1[5][5], int8_t arr2[5][5]);
+//multiplica duas matrizes5x5 de 8 bits e retorna uma de 16
+int mul_completo(int8_t arr1[5][5], int8_t arr2[5][5], int16_t arr3[5][5]);
 //printa uma matrix 5x5
 int print_matrix5x5(int8_t arr1[5][5]);
+//printa uma matriz 5x5 de 16 bits cada numero
+int print_matrix5x5_16b(int16_t arr1[5][5]);
 
 #endif
