1f62f40f96febd160a234577cc96ceb1fbb455f2
Giu <120753646+Giu-11@users.noreply.github.com>
2025-05-31 11:06:32 -0300

    ✨feat: menu para os filtros

--- main.c
+++ main.c
@@ -34,14 +34,108 @@ void roberts(imagem* img);
 void laplace(imagem* img);
 
 
-int main(){
-    imagem img;
-    leitorBmp("imagens/input/1.bmp", &img);
-    //printf("altura: %d\nlargura: %d\nprofundidade: %d\n", img.altura, img.largura, img.profundidade);
-    roberts(&img);
-    salvarBmp("imagens/output/1Out.bmp", &img);
-    printf("fim");
+int main() {
+    int opcaoImagem = 0;
+    int opcaoFiltro = 0;
+    char caminhoImagem[100];
+    char caminhoSaida[120];
+    char nomeFiltro[20];
+    int sair = 0;
+
+    while (!sair) {
+        printf("\n=== MENU PRINCIPAL ===\n");
+        printf("1. Selecionar Imagem\n");
+        printf("2. Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcaoImagem);
+
+        if (opcaoImagem == 2) {
+            sair = 1;
+            printf("\nSaindo do programa...\n\n");
+            break;
+        }
+
+        // Escolha da imagem
+        printf("\nEscolha uma imagem:\n");
+        printf("1. 1.bmp\n");
+        printf("2. 2.bmp\n");
+        printf("3. 3.bmp\n");
+        printf("Escolha: ");
+        scanf("%d", &opcaoImagem);
+
+        switch (opcaoImagem) {
+            case 1:
+                snprintf(caminhoImagem, sizeof(caminhoImagem), "imagens/input/1.bmp");
+                break;
+            case 2:
+                snprintf(caminhoImagem, sizeof(caminhoImagem), "imagens/input/2.bmp");
+                break;
+            case 3:
+                snprintf(caminhoImagem, sizeof(caminhoImagem), "imagens/input/3.bmp");
+                break;
+            default:
+                printf("Opcao invalida! Voltando ao menu principal...\n\n");
+                continue;
+        }
+
+        printf("Imagem selecionada: %s\n", caminhoImagem);
+
+        imagem img;
+
+        leitorBmp(caminhoImagem, &img);
+
+        // Escolha do filtro
+        printf("\nEscolha um filtro para aplicar:\n");
+        printf("1. Sobel 3x3\n");
+        printf("2. Sobel 5x5\n");
+        printf("3. Prewitt\n");
+        printf("4. Roberts\n");
+        printf("5. Laplace\n");
+        printf("Escolha: ");
+        scanf("%d", &opcaoFiltro);
+
+        switch (opcaoFiltro) {
+            case 1:
+                strcpy(nomeFiltro, "sobel3x3");
+                printf("Filtro Sobel 3x3 selecionado.\n");
+                sobel3x3(&img);
+                break;
+            case 2:
+                strcpy(nomeFiltro, "sobel5x5");
+                printf("Filtro Sobel 5x5 selecionado.\n");
+                sobel5x5(&img);
+                break;
+            case 3:
+                strcpy(nomeFiltro, "prewitt");
+                printf("Filtro Prewitt selecionado.\n");
+                perwitt(&img);
+                break;
+            case 4:
+                strcpy(nomeFiltro, "roberts");
+                printf("Filtro Roberts selecionado.\n");
+                roberts(&img);
+                break;
+            case 5:
+                strcpy(nomeFiltro, "laplace");
+                printf("Filtro Laplace selecionado.\n");
+                laplace(&img);
+                break;
+            default:
+                printf("Opcao de filtro invalida!\n");
+                continue;
+        }
+
+        // Criar caminho de saída
+        snprintf(caminhoSaida, sizeof(caminhoSaida), "imagens/output/%d%s.bmp", opcaoImagem, nomeFiltro);
+
+        salvarBmp(caminhoSaida, &img);
+
+        printf("\nImagem salva em: %s\n\n", caminhoSaida);
+    }
+
+    return 0;
 }
+
 
 void sobel3x3(imagem* img){
 
@@ -497,7 +591,6 @@ void leitorBmp(char* nomeArquivoLeitura, imagem* img) {
 
     int pixelDataOffset;
     memcpy(&pixelDataOffset, &headerTemp[10], sizeof(int));
-    printf("?pixeldataoffset: %d\n", pixelDataOffset);
 
     // Aloca e lê o cabeçalho completo até o início dos dados de pixel
     unsigned char* header = malloc(pixelDataOffset * sizeof(unsigned char));
@@ -524,8 +617,6 @@ void leitorBmp(char* nomeArquivoLeitura, imagem* img) {
     fread(buf, sizeof(unsigned char), imageSize, streamIn);
     fclose(streamIn);
 
-    printf("altura: %d\nlargura: %d\nprofundidade: %d\n", height, width, bitDepth);
-
     // Converte em tons de cinza
     for (int i = 0; i < height; i++) {
         int linha = height - i - 1;
@@ -550,11 +641,6 @@ void leitorBmp(char* nomeArquivoLeitura, imagem* img) {
     img->cabecalho = header;
 
     img->pixels = buf;
-
-    // Impressão opcional de debug
-    printf("altura: %d\nlargura: %d\nprofundidade: %d\n", width, height, bitDepth);
-    printf("Compressao: %d\n", compression);
-    printf("Offset: %d\n", pixelDataOffset);
 }
 
 
