4f152df61230b5966fe9878730316a2feb64a343
Tamilly costa <Tamillycosta899@gmail.com>
2025-05-31 22:37:18 -0300

    feat: implementa menu

--- Biblioteca/Lib/Headers/assemblyFunc.h
+++ Biblioteca/Lib/Headers/assemblyFunc.h
@@ -10,7 +10,6 @@ extern int8_t overflow_values[25];
 extern int overflow_count;
 
 
-
 int init_hardware(); // inicia mapeamento de memoria 
 void send_element(int8_t value, int i, int j, int opcode); // envia os elementos das matrizes de 8 a 8 bits
 void execute_operation(int opcode); // opcode da operação aritimetica
---
+++ Biblioteca/main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include "Lib/Headers/assemblyFunc.h"
+#include "Lib/Headers/functions.h"
+
+
+int main(){
+    start();
+    return 0;
+}
